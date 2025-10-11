6d69cf4f2057f329a92432bf2b42114d8c2edd88
StanislawPalen <spalen@edu.cdv.pl>
2025-10-11 16:33:02 +0200

    Update Program.cs

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,3 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
+/* ZADANKO ze sklepem
+ const int requiredAge = 16;
+const int beerRequiredAge = 18;
 
-Console.WriteLine("Hello, World!");
+const string accessDeniedMessage = "You must be at least 16 years old.";
+const string accessAllowedMessage = "Welcome to my shop.";
+const string beerRestrictionMessage = "You must be 18 years old to buy a beer.";
+
+int age = 14;
+
+if (age < requiredAge)
+{
+    if (age >= 14)
+    {
+        Console.WriteLine($"{accessAllowedMessage} {beerRestrictionMessage}");
+    }
+    else
+    {
+        Console.WriteLine(accessDeniedMessage);
+    }
+}
+else if (age < beerRequiredAge)
+{
+    Console.WriteLine(accessAllowedMessage);
+}
+else
+{
+    Console.WriteLine($"{accessAllowedMessage} {beerRestrictionMessage}");
+}
+*/
+
+/* ZADANIE 1
+ string password;
+do
+{
+    Console.Write("Podaj hasło: ");
+    password = Console.ReadLine();
+}
+while (password != "admin123");
+Console.WriteLine("Zalogowano pomyślnie!");
+*/
+
+/* ZADANIE 2
+ string number;
+do
+{
+    Console.Write("Podaj liczbę: ");
+    int.TryParse(Console.ReadLine(), out number);
+}
+while (number >= 0);
+Console.WriteLine("Prawidłowa liczba!");
+*/
+
+/* ZADANIE 3 
+string[] cities = { "Poznań", "Warszawa", "Gniezno", "Gdańsk", "Kraków" };
+foreach (var city in cities)
+{
+    Console.WriteLine(city);
+}
+*/
+
+/* ZADANIE 4 */
+
+
+/* ZADANIE 5 */
+
+
+/* ZADANIE 6 */
+
+
+/* ZADANIE 7 */
